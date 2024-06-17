6e10dace9630bf5900bdfddeca8f08c7eed8a09b
amarchan-stratio <amarchan@stratio.com>
2024-06-17 22:19:35 +0200

    New match

--- app/app.py
+++ app/app.py
@@ -484,6 +484,9 @@ def load_matches(x):
             elif match_tag == 'Bélgica-Eslovaquia':
                 home_score = 0
                 away_score = 1
+            elif match_tag == 'Austria-Francia':
+                home_score = 0
+                away_score = 1
 
             row = {
                 'date': date.strftime('%a, %d %b, %H:%M').title(),
